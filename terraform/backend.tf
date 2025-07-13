5f1d1d0ee4d137f8e096854a9f4b7c267f0ae3ef
pavankurugodu <pavanpavi56@gmail.com>
2025-07-13 20:01:28 +0530

    changes made to tf files

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "mysampleawsbucketforterrformstatefile"
-    key    = "backend-locking"
+    key    = "backend-locking/terraform.tfstate"
     region = "ap-south-1"
     use_lockfile = true
   }
